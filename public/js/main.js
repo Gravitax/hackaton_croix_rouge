2cb19650bbef483148cd0adc6a2b966e3ac60e15
Marc-antoine BOYE <maboye@e1r2p6.clusters.42paris.fr>
2022-10-15 12:03:42 +0200

    origin

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,17 @@
 import mab from "../mab_framework/mab.js";
 
 
+const	__token = "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzQ5OTgyN2FmZGI3NzgzOWEyNmY2MDMiLCJpYXQiOjE2NjU3Njc5MTYsImV4cCI6MTY5NzMwMzkxNn0.UMjmas1ZyJlaXSS8S90vqdr7j3x_suHJMqM4EUD4_Y4";
+
 const	app = () => {
 
-	console.log("app");
+	fetch("https://api.prosoliguide.fr/place/0?lang=fr", {
+		headers : {
+			"Authorization" : __token
+		}
+	})
+		.then((data) => data.text())
+		.then((data_text) => console.log(data_text));
 
 };
 
