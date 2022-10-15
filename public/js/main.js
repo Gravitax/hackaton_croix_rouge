fcb365f1d81e8d24606377c1fad013d0130d2944
Marc-antoine BOYE <maboye@e1r2p6.clusters.42paris.fr>
2022-10-15 11:47:07 +0200

    origin

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,16 @@
-console.log("main");
+import mab from "../mab_framework/mab.js";
+
+
+const	app = () => {
+
+	console.log("app");
+
+};
+
+mab.init();
+
+mab(document).ready(() => {
+
+	app();
+
+});
